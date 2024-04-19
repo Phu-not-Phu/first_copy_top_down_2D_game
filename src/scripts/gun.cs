0ac2db5372b2ea66109d5bcb84abdab3359f4b2a
Phu-not-Phu <danghomp@gmail.com>
2024-04-19 11:32:04 +0700

    Flip Gun if enemy global position smaller than gun global position

--- src/scripts/gun.cs
+++ src/scripts/gun.cs
@@ -4,10 +4,12 @@ using System;
 public partial class gun : Area2D
 {
     static Marker2D shootingPoint;
+    AnimatedSprite2D sprite;
 
     public override void _Ready()
     {
         shootingPoint = GetNode<Marker2D>("%ShootingPoint");
+        sprite = GetNode<AnimatedSprite2D>("WeaponPivot/AnimatedSprite2D");
         Timer timer = GetNode<Timer>("Timer");
         timer.Timeout += () => Shoot();
     }
@@ -19,6 +21,18 @@ public partial class gun : Area2D
         {
             var target_enemy = enemies_in_range[0];
             LookAt(target_enemy.GlobalPosition);
+
+            GD.Print("enemy position: " + target_enemy.GlobalPosition.X); 
+            GD.Print("gun position: " + GlobalPosition.X);
+
+            if(target_enemy.GlobalPosition.X < GlobalPosition.X)
+            {
+                sprite.FlipV = true;
+            }
+            else
+            {
+                sprite.FlipV = false;
+            }
         }
     }
 
