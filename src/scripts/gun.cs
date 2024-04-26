b629ca803f221eb908f1609d8ded1f032e8900c3
Phu-not-Phu <danghomp@gmail.com>
2024-04-26 23:10:23 +0700

    Change JSON direction, separate file color and asssets file into many types

--- src/scripts/gun.cs
+++ src/scripts/gun.cs
@@ -3,47 +3,47 @@ using System;
 
 public partial class gun : Area2D
 {
-    static Marker2D shootingPoint;
-    AnimatedSprite2D sprite;
-    AudioStreamPlayer2D gunSFX;
+	static Marker2D shootingPoint;
+	AnimatedSprite2D sprite;
+	AudioStreamPlayer2D gunSFX;
 
-    public override void _Ready()
-    {
-        shootingPoint = GetNode<Marker2D>("%ShootingPoint");
-        sprite = GetNode<AnimatedSprite2D>("WeaponPivot/AnimatedSprite2D");
-        gunSFX = GetNode<AudioStreamPlayer2D>("Gun_SFX");
-    }
+	public override void _Ready()
+	{
+		shootingPoint = GetNode<Marker2D>("%ShootingPoint");
+		sprite = GetNode<AnimatedSprite2D>("WeaponPivot/AnimatedSprite2D");
+		gunSFX = GetNode<AudioStreamPlayer2D>("Gun_SFX");
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        var mouse_pos = GetGlobalMousePosition();
-        LookAt(mouse_pos);
+	public override void _PhysicsProcess(double delta)
+	{
+		var mouse_pos = GetGlobalMousePosition();
+		LookAt(mouse_pos);
 
-        if (Input.IsActionPressed("shoot"))
-        {
-            Shoot();
-        }
+		if (Input.IsActionPressed("shoot"))
+		{
+			Shoot();
+		}
 
-        if (mouse_pos.X < GlobalPosition.X)
-        {
-            sprite.FlipV = true;
-        }
-        else
-        {
-            sprite.FlipV = false;
-        }
-    }
+		if (mouse_pos.X < GlobalPosition.X)
+		{
+			sprite.FlipV = true;
+		}
+		else
+		{
+			sprite.FlipV = false;
+		}
+	}
 
-    public void Shoot()
-    {
-        gunSFX.Play();
+	public void Shoot()
+	{
+		gunSFX.Play();
 
-        PackedScene bullet = ResourceLoader.Load<PackedScene>("res://src/scenes/bullet.tscn");
-        var new_bullet = (Node2D)bullet.Instantiate();
+		PackedScene bullet = ResourceLoader.Load<PackedScene>("res://src/scenes/bullet.tscn");
+		var new_bullet = (Node2D)bullet.Instantiate();
 
-        new_bullet.GlobalPosition = shootingPoint.GlobalPosition;
-        new_bullet.GlobalRotation = shootingPoint.GlobalRotation;
+		new_bullet.GlobalPosition = shootingPoint.GlobalPosition;
+		new_bullet.GlobalRotation = shootingPoint.GlobalRotation;
 
-        shootingPoint.AddChild(new_bullet);
-    }
+		shootingPoint.AddChild(new_bullet);
+	}
 }
--- src/scripts/mouse_cursor.cs
+++ src/scripts/mouse_cursor.cs
@@ -3,25 +3,25 @@ using System;
 
 public partial class mouse_cursor : Node2D
 {
-    [Export] float cursor_scaleX;
-    [Export] float cursor_scaleY;
-    Texture empty_cursor = null;
-    AnimatedSprite2D cursor_sprite;
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Hidden;
-        cursor_sprite = GetNode<AnimatedSprite2D>("CanvasLayer/AnimatedSprite2D");
-        cursor_sprite.ApplyScale(new Vector2(cursor_scaleX, cursor_scaleY));
-    }
+	[Export] float cursor_scaleX;
+	[Export] float cursor_scaleY;
+	Texture empty_cursor = null;
+	AnimatedSprite2D cursor_sprite;
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Hidden;
+		cursor_sprite = GetNode<AnimatedSprite2D>("CanvasLayer/AnimatedSprite2D");
+		cursor_sprite.ApplyScale(new Vector2(cursor_scaleX, cursor_scaleY));
+	}
 
-    public override void _Process(double delta)
-    {
-        var mouse_pos = GetGlobalMousePosition();
-        cursor_sprite.GlobalPosition = mouse_pos;
+	public override void _Process(double delta)
+	{
+		var mouse_pos = GetGlobalMousePosition();
+		cursor_sprite.GlobalPosition = mouse_pos;
 
-        if (Input.IsActionJustPressed("escape"))
-        {
-            Input.MouseMode = Input.MouseModeEnum.Confined;
-        }
-    }
+		if (Input.IsActionJustPressed("escape"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Confined;
+		}
+	}
 }
