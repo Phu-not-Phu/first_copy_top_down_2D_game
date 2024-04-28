9faa2c6a49f9b81720f225fd3d3aa31f4a2bf1e6
Phu-not-Phu <danghomp@gmail.com>
2024-04-28 13:50:31 +0700

    Add Shotgun and animation player, change sfx play in animation player, add shot to animation player shotgun

--- src/scripts/gun.cs
+++ src/scripts/gun.cs
@@ -5,13 +5,14 @@ public partial class gun : Area2D
 {
 	static Marker2D shootingPoint;
 	AnimatedSprite2D sprite;
-	AudioStreamPlayer2D gunSFX;
+	AnimationPlayer animPlayer;
+	bool isShooting = false;
 
 	public override void _Ready()
 	{
 		shootingPoint = GetNode<Marker2D>("%ShootingPoint");
 		sprite = GetNode<AnimatedSprite2D>("WeaponPivot/AnimatedSprite2D");
-		gunSFX = GetNode<AudioStreamPlayer2D>("Gun_SFX");
+		animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -19,9 +20,12 @@ public partial class gun : Area2D
 		var mouse_pos = GetGlobalMousePosition();
 		LookAt(mouse_pos);
 
-		if (Input.IsActionPressed("shoot"))
+		if (isShooting == false)
 		{
-			Shoot();
+			if (Input.IsActionPressed("shoot"))
+			{
+				animPlayer.Play("shoot");
+			}
 		}
 
 		if (mouse_pos.X < GlobalPosition.X)
@@ -36,14 +40,34 @@ public partial class gun : Area2D
 
 	public void Shoot()
 	{
-		gunSFX.Play();
-
+		SetShootingTrue();
+		
 		PackedScene bullet = ResourceLoader.Load<PackedScene>("res://src/scenes/bullet.tscn");
-		var new_bullet = (Node2D)bullet.Instantiate();
+		var new_bullet_middle = (Node2D)bullet.Instantiate();
+		var new_bullet_top = (Node2D)bullet.Instantiate();
+		var new_bullet_bottom = (Node2D)bullet.Instantiate();
 
-		new_bullet.GlobalPosition = shootingPoint.GlobalPosition;
-		new_bullet.GlobalRotation = shootingPoint.GlobalRotation;
+		new_bullet_middle.GlobalPosition = shootingPoint.GlobalPosition;
+		new_bullet_middle.GlobalRotation = shootingPoint.GlobalRotation;
 
-		shootingPoint.AddChild(new_bullet);
+		new_bullet_top.GlobalPosition = shootingPoint.GlobalPosition;
+		new_bullet_top.GlobalRotation = shootingPoint.GlobalRotation + Mathf.DegToRad(-5);
+
+		new_bullet_bottom.GlobalPosition = shootingPoint.GlobalPosition;
+		new_bullet_bottom.GlobalRotation = shootingPoint.GlobalRotation + Mathf.DegToRad(5);
+
+		shootingPoint.AddChild(new_bullet_top);
+		shootingPoint.AddChild(new_bullet_middle);
+		shootingPoint.AddChild(new_bullet_bottom);
+	}
+
+	public void SetShootingTrue()
+	{
+		isShooting = true;
+	}	
+
+	public void SetShootingFalse()
+	{
+		isShooting = false;
 	}
 }
